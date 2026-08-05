6e247e2617ad966aee53833d89a65d0258ab2dd5
rohithpalyamworkmail-dev <rohithpalyamworkmail@gmail.com>
2026-08-05 13:40:13 +0530

    Update mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -11,7 +11,7 @@ from pymongo import MongoClient
 
 # Initialize MongoDB connection
 if "client" not in st.session_state:
-    st.session_state["client"] = MongoClient("mongodb://localhost:27017/")
+    st.session_state["client"] = MongoClient("mongodb+srv://rohith_palyam:rohith_palyam@cluster0.9q8c1if.mongodb.net/?appName=cluster0")
     st.session_state["db"] = st.session_state["client"]["courses_db"]
     st.session_state["collection"] = st.session_state["db"]["course_collection"]
 
