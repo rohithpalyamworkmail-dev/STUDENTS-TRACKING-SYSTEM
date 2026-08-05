604bceaa936166e8e04957762b51143454fb5254
rohithpalyamworkmail-dev <rohithpalyamworkmail@gmail.com>
2026-08-05 13:39:49 +0530

    Update mongodb1.py

--- mongodb1.py
+++ mongodb1.py
@@ -2,6 +2,6 @@ from pymongo import MongoClient
 import streamlit as st
 
 if "client" not in st.session_state:
-    st.session_state["client"]=MongoClient("mongodb://localhost:27017/")
+    st.session_state["client"]=MongoClient("mongodb+srv://rohith_palyam:rohith_palyam@cluster0.9q8c1if.mongodb.net/?appName=cluster0")
     st.session_state["db"]=st.session_state["client"]["courses_db"]
     st.session_state["collection"]=st.session_state["db"]["course_collection"]
